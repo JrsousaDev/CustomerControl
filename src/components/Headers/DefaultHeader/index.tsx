a692ba142285385a3dd745279ae9bcab9220dffd
Junior Sousa <jrsousadev@gmail.com>
2022-04-05 00:03:13 -0300

    fix

--- src/components/Headers/DefaultHeader/index.tsx
+++ src/components/Headers/DefaultHeader/index.tsx
@@ -1,5 +1,3 @@
-import { IconNotify } from "../../Icons/Icons";
-import { InputSearch } from "../../Inputs/InputSearch";
 import { IDefaultHeaderProps } from "./IDefaultHeader";
 import { 
   Container,
@@ -18,12 +16,12 @@ export default function DefaultHeader({ title, className }: IDefaultHeaderProps)
       </C_Text>
 
       <C_Icons>
-        <InputSearch 
+        {/* <InputSearch 
           styleContainer={{maxWidth: '15rem'}}
           placeholder="Pesquisar" 
           icon={true}
         />
-        <IconNotify styleIcon={{height: '25px', width: '25px', cursor: 'pointer'}}/>
+        <IconNotify styleIcon={{height: '25px', width: '25px', cursor: 'pointer'}}/> */}
         {/* <IconUser styleIcon={{height: '25px', width: '20px', cursor: 'pointer'}}/> */}
       </C_Icons>
 
--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -1,10 +1,14 @@
 //import { RiUserFill } from "react-icons/Ri";
-import { IoIosNotifications } from "react-icons/Io";
-import { AiOutlineSearch } from "react-icons/Ai";
+//import { IoIosNotifications } from "react-icons/Io";
+//import { AiOutlineSearch } from "react-icons/Ai";
 
-interface IconsProps {
+/* interface IconsProps {
   styleIcon?: any;
-} 
+}  */
 //export const IconUser = ({styleIcon}: IconsProps) => <RiUserFill style={styleIcon}/>
-export const IconNotify = ({styleIcon}: IconsProps) => <IoIosNotifications style={styleIcon} />
-export const IconSearch = ({styleIcon}: IconsProps) => <AiOutlineSearch style={styleIcon} />
+//export const IconNotify = ({styleIcon}: IconsProps) => <IoIosNotifications style={styleIcon} />
+//export const IconSearch = ({styleIcon}: IconsProps) => <AiOutlineSearch style={styleIcon} />
+
+export default function Icons() {
+  return<div>HelloWorld</div>
+}

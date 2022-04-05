d2a0984181f02a3131904a6efc6b5d577151946e
Junior Sousa <jrsousadev@gmail.com>
2022-04-05 00:25:37 -0300

    Joao fixed my projecty

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -1,6 +1,6 @@
-/* import { RiUserFill } from "react-icons/Ri";
-import { IoIosNotifications } from "react-icons/Io";
-import { AiOutlineSearch } from "react-icons/Ai";
+import { RiUserFill } from "react-icons/ri";
+import { IoIosNotifications } from "react-icons/io";
+import { AiOutlineSearch } from "react-icons/ai";
 
 interface IconsProps {
   styleIcon?: any;
@@ -8,8 +8,3 @@ interface IconsProps {
 export const IconUser = ({styleIcon}: IconsProps) => <RiUserFill style={styleIcon}/>
 export const IconNotify = ({styleIcon}: IconsProps) => <IoIosNotifications style={styleIcon} />
 export const IconSearch = ({styleIcon}: IconsProps) => <AiOutlineSearch style={styleIcon} />
- */
-
-export default function Icons() {
-  return<div>Oi</div>
-}

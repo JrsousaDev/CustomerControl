06c47b640f0aecde794a119af10b91d35f504a2f
Junior Sousa <jrsousadev@gmail.com>
2022-04-21 16:00:08 -0300

    fix

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -4,4 +4,5 @@ export const AUTH_USER = "user/login";
 
 //  CUSTOMER  //
 export const UPDATE_DUEDATE_CUSTOMER = "customer/update-dueDate";
+export const DELETE_CUSTOMER = "customer/delete";
 export const CREATE_CUSTOMER = "customer/create";
